2cbbd329d7b5582f88f41b3de9ff064a317c4dd9
顶峰云雪 <pinus0716@163.com>
2022-08-10 22:59:17 +0800

    fix:添加suspense否则报错

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import BasicLayout from './layouts/BasicLayout'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { NavItems } from './router/router'
@@ -7,20 +8,22 @@ import React, { useState } from 'react'
 function App() {
 	return (
 		<BasicLayout>
-			<Routes>
-				{/* 菜单栏 */}
-				{NavItems.map((item, index) => {
-					return <Route path={item.key} element={item.component} key={index}></Route>
-				})}
-				{/* 文章详情 */}
-				<Route
-					path={'/articles/:id'}
-					element={<ArticleDetail />}
-					key={'article-detail'}
-				></Route>
-				{/* 将'/'重定向 */}
-				<Route path='/' element={<Navigate to='/home' />}></Route>
-			</Routes>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					{/* 菜单栏 */}
+					{NavItems.map((item, index) => {
+						return <Route path={item.key} element={item.component} key={index}></Route>
+					})}
+					{/* 文章详情 */}
+					<Route
+						path={'/articles/:id'}
+						element={<ArticleDetail />}
+						key={'article-detail'}
+					></Route>
+					{/* 将'/'重定向 */}
+					<Route path='/' element={<Navigate to='/home' />}></Route>
+				</Routes>
+			</Suspense>
 		</BasicLayout>
 	)
 }
